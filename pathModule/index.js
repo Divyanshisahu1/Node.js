48656a98ed2783fb9b2673eacd32f4412316f0bf
Divyanshisahu01 <divyanshi.sahu@wehealapp.in>
2022-12-18 15:18:39 +0530

    path Module

---
+++ pathModule/index.js
@@ -0,0 +1,5 @@
+const path = require("path")
+console.log(path.dirname("D:\StudyMaterial\NadosLearning\Node.js\pathModule\index.js"))
+console.log(path.basename("D:\StudyMaterial\NadosLearning\Node.js\pathModule\index.js"))
+console.log(path.extname("D:\StudyMaterial\NadosLearning\Node.js\pathModule\index.js"))
+console.log(path.parse("D:\StudyMaterial\NadosLearning\Node.js\pathModule\index.js"))
